1c72260a7f66bb0bcf5cfe07e94292664c56fb85
Jagadeesh Pradhani <97280653+Jagadeesh-pradhani@users.noreply.github.com>
2024-11-18 20:12:15 +0530

    Full commit

---
+++ launch/simulate.launch.py
@@ -0,0 +1,78 @@
+
+"""Launch an iris quadcopter in Gazebo"""
+from pathlib import Path
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import os
+
+def generate_launch_description():
+    """Generate a launch description for a iris quadcopter."""
+    pkg_project_bringup = get_package_share_directory("custom_drone")
+    pkg_project_gazebo = get_package_share_directory("ardupilot_gz_gazebo")
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
+
+
+
+    # Gazebo.
+    gz_sim_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            f'{Path(pkg_ros_gz_sim) / "launch" / "gz_sim.launch.py"}'
+        ),
+        launch_arguments={
+            "gz_args": "-v4 -s -r "
+            f'{Path(pkg_project_bringup) / "worlds" / "iris_runway.sdf"}'
+        }.items(),
+    )
+
+    gz_sim_gui = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            f'{Path(pkg_ros_gz_sim) / "launch" / "gz_sim.launch.py"}'
+        ),
+        launch_arguments={"gz_args": "-v4 -g"}.items(),
+    )
+
+    # Bridge.
+    bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        parameters=[
+            {
+                "config_file": os.path.join(
+                    pkg_project_bringup, "config", "bridge.yaml"
+                ),
+                "qos_overrides./tf_static.publisher.durability": "transient_local",
+            }
+        ],
+        output="screen",
+    )
+
+    # RViz.
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", f'{Path(pkg_project_bringup) / "rviz" / "iris.rviz"}'],
+        condition=IfCondition(LaunchConfiguration("rviz")),
+    )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "rviz", default_value="true", description="Open RViz."
+            ),
+            gz_sim_server,
+            gz_sim_gui,
+            # rviz,
+            bridge,
+        ]
+    )
---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'custom_drone'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*launch.py")),
+        ),
+        (
+            os.path.join("share", package_name, "config"),
+            glob(os.path.join("config", "*.yaml")),
+        ),
+        (
+            os.path.join("share", package_name, "worlds"),
+            glob(os.path.join("worlds", "*.sdf")),
+        ),
+        (
+            os.path.join("share", package_name, "rviz"),
+            glob(os.path.join("rviz", "*.rviz")),
+        ),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='intel',
+    maintainer_email='01fe21bee114@kletech.ac.in',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    # tests_require=['pytest'],
+    extras_require={'test':['pytest'],},
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
